5c5660d8ca7e3d3c99b68a75e7cf29e120e218e3
Vitrixxl <vitrie91@gmail.com>
2024-06-05 10:26:02 +0200

    Finaly fixed fucking prisma ?

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-
+// Declare a global variable for Prisma
 declare global {
-    var prisma: PrismaClient | undefined
+    var prisma: PrismaClient | undefined;
 }
+
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
+    prisma = new PrismaClient();
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient()
+        global.prisma = new PrismaClient();
     }
-    prisma = global.prisma
+    prisma = global.prisma;
 }
 
-export default prisma
+export default prisma;
